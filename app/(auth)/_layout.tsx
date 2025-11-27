d5cea92b57fb4d8be4f10f6cb840583d43c78d1e
minhdung321 <duychating@gmail.com>
2025-11-28 00:55:54 +0700

    Version 0.2.1 Cải thiện sự mượt mà và đồng nhất màu nền của hiệu ứng khi chuyển qua lại giữa hai màn hình SignIn và SignUp.

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,10 +1,21 @@
 import { Stack } from "expo-router";
+import { useColorScheme } from "react-native";
 
 export default function AuthLayout() {
-    return (
-        <Stack screenOptions={{headerShown: false, animation: 'slide_from_right'}}>
-            <Stack.Screen name="sign-in" />
-            <Stack.Screen name="sign-up" />
-        </Stack>
-    )
+  const colorScheme = useColorScheme();
+  const backgroundColor = colorScheme === "dark" ? "#000" : "#fff";
+
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+        animation: "slide_from_right",
+        contentStyle: { backgroundColor },
+        animationDuration: 250,
+      }}
+    >
+      <Stack.Screen name="sign-in" />
+      <Stack.Screen name="sign-up" />
+    </Stack>
+  );
 }
