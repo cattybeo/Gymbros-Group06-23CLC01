465aedf31dea9cadc8f36bef4bee1313e1e631c2
minhdung321 <duychating@gmail.com>
2025-12-15 22:50:45 +0700

    feat: initialize Supabase client with AsyncStorage and remove the changelog.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,26 +1,16 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient, processLock } from "@supabase/supabase-js";
-import { Platform } from "react-native";
 import "react-native-url-polyfill/auto";
 
 // Tạo một Storage Adapter để tránh lỗi trên Server (Node.js)
 const ExpoStorage = {
   getItem: (key: string) => {
-    if (Platform.OS === "web" && typeof window === "undefined") {
-      return Promise.resolve(null); // Đang chạy trên Server -> Trả về null
-    }
     return AsyncStorage.getItem(key);
   },
   setItem: (key: string, value: string) => {
-    if (Platform.OS === "web" && typeof window === "undefined") {
-      return Promise.resolve(); // Đang chạy trên Server -> Không làm gì cả
-    }
     return AsyncStorage.setItem(key, value);
   },
   removeItem: (key: string) => {
-    if (Platform.OS === "web" && typeof window === "undefined") {
-      return Promise.resolve(); // Đang chạy trên Server -> Không làm gì cả
-    }
     return AsyncStorage.removeItem(key);
   },
 };
